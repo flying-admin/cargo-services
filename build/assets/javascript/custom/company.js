7c91e1ad941deb21f33a36cdcaec413384108e63
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-05-24 10:46:57 +0200

    build

---
+++ build/assets/javascript/custom/company.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){if($(".content-slider").length>0){new Swiper(".content-slider .swiper-container",{spaceBetween:10,slidesPerView:2,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},breakpoints:{990:{slidesPerView:3},767:{slidesPerView:1.3}}})}});

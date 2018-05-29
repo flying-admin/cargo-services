c5817e1594934a73f1f600980fa60b8dbee1b5d1
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-05-29 17:11:18 +0200

    build

---
+++ build/assets/javascript/custom/news.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){if($(".news-slider").length>0){new Swiper(".news-slider .swiper-container",{spaceBetween:20,slidesPerView:1.4,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}})}});
